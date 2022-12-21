9942b6cd3a682836e49e3af314a610c4772cb49e
Simon Chabrier <simonchabrier@gmail.com>
2022-12-21 19:03:13 +0100

    ajout d'une route api pour user login

--- src/Controller/ApiController.php
+++ src/Controller/ApiController.php
@@ -313,6 +313,44 @@ class ApiController extends AbstractController
         );
     }
 
+    /**
+     * Permet de se connecter
+     * @Route("/api/login", name="api_user_login", methods={"POST"})
+     */
+
+    public function apiUserLogin(
+        EntityManagerInterface $doctrine,
+        Request $request,
+        SerializerInterface $serializer,
+        ValidatorInterface $validator
+    ): Response
+    {
+        $data = $request->getContent();
+        $user = $serializer->deserialize($data, User::class, 'json');
+        $errors = $validator->validate($user);
+
+        // login login 
+        $user = $doctrine->getRepository(User::class)->findOneBy(['email' => $user->getEmail()]);
+        if (!$user) {
+            return new JsonResponse('Email inconnu', Response::HTTP_UNPROCESSABLE_ENTITY);
+        }
+
+        // login password
+        if (!password_verify($user->getPassword(), $user->getPassword())) {
+            return new JsonResponse('Mot de passe incorrect', Response::HTTP_UNPROCESSABLE_ENTITY);
+        }
+
+        return $this->json(
+            $user,
+            Response::HTTP_OK,
+            [],
+            ['groups' => ['user_write']]
+        );
+
+
+
+    }
+
 
 
 }
